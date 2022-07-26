53483e72b8545df800157c4a00ddd3a2aab2eb8a
Mohantysuman <105624690+Mohantysuman@users.noreply.github.com>
2022-07-26 16:27:39 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_26_SM.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //GET: EmployeeController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: EmployeeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Employeepage()
+        {
+            return View();
+        }
+        //GET: EmployeeController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment_26_SM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
